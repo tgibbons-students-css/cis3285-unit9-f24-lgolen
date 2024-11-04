3a22acfd35e1a53e1af2cc8962b29c9c34c6f547
Lonnie Golen <lgolen@css.edu>
2024-11-03 22:04:30 -0600

    405 done

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -9,11 +9,22 @@ namespace SingleResponsibilityPrinciple
         public void LogWarning(string message, params object[] args)
         {
             Console.WriteLine(string.Concat("WARN: ", message), args);
+            LogMessage("WARN", message, args);
         }
 
         public void LogInfo(string message, params object[] args)
         {
             Console.WriteLine(string.Concat("INFO: ", message), args);
+            LogMessage("INFO", message, args);
+        }
+
+        private void LogMessage(string type, string message, params object[] args)
+        {
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log><type>" + type + "</type><message>" + message + "</message></log> ", args);
+            }
         }
     }
 }
+
