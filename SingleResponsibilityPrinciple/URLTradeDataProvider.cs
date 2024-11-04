faf9b1622bc57350ea7ba6529e909788438da3f8
Lonnie Golen <lgolen@css.edu>
2024-11-03 21:44:29 -0600

    407 done

--- SingleResponsibilityPrinciple/URLTradeDataProvider.cs
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,13 +10,44 @@ namespace SingleResponsibilityPrinciple
 {
     public class URLTradeDataProvider : ITradeDataProvider
     {
+        string url;
+        ILogger logger;
+
         public URLTradeDataProvider(string url, ILogger logger)
         {
+            this.url = url;
+            this.logger = logger;
         }
 
         public IEnumerable<string> GetTradeData()
         {
-            throw new NotImplementedException();
+            List<string> tradeData = new List<string>();
+            logger.LogInfo("Reading trades from URL: " + url);
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    // log error and throw an exception if the URL fails
+                    logger.LogInfo("Failed to read URL.");
+                    throw new HttpRequestException();
+                }
+                // set up a Stream and StreamReader to access the data
+                using (Stream stream = response.Content.ReadAsStreamAsync().Result)
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        tradeData.Add(line);
+                        logger.LogInfo("added line");
+                    }
+                }
+
+               
+            }
+            return tradeData;
         }
     }
 }
