62c5baafea7be7f262ed705718f71687dd44f18d
Lonnie Golen <lgolen@css.edu>
2024-11-03 21:45:17 -0600

    409 done

--- SingleResponsibilityPrinciple/RestfulTradeDataProvider.cs
+++ SingleResponsibilityPrinciple/RestfulTradeDataProvider.cs
@@ -2,20 +2,50 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SingleResponsibilityPrinciple
 {
     public class RestfulTradeDataProvider : ITradeDataProvider
     {
+        string url;
+        ILogger logger;
+        private readonly HttpClient _httpClient;
+
         public RestfulTradeDataProvider(string url, ILogger logger)
         {
+            this.url = url;
+            this.logger = logger;
+            _httpClient = new HttpClient();
         }
 
         public IEnumerable<string> GetTradeData()
         {
-            throw new NotImplementedException();
+            // URL for the API endpoint
+            string url = "https://unit9trader.azurewebsites.net/api/TradeData";
+
+            // Send the GET request and wait for the result
+            var response = _httpClient.GetAsync(url).Result;
+
+            // Ensure the response was successful
+            if (!response.IsSuccessStatusCode)
+            {
+                // log error and throw an exception if the URL fails
+                logger.LogInfo("Failed to read URL.");
+                throw new HttpRequestException();
+            }
+
+            // Read the response content as a string synchronously
+            string jsonString = response.Content.ReadAsStringAsync().Result;
+
+            // Deserialize the JSON string into a list of strings
+            List<string> trades = JsonSerializer.Deserialize<List<string>>(jsonString);
+
+            return trades;
         }
+
     }
 }
